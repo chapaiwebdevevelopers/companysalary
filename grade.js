24b3174a8e1b1e827b3f0dd53c944c6e6b3e17ee
chapaiwebdevelopers <chapaiwebdevelopers@gmail.com>
2026-07-18 15:27:45 +0600

    Grading System update

---
+++ grade.js
@@ -0,0 +1,23 @@
+const marks= 0;
+
+if(marks <= 100){
+    if (marks >= 80){
+    console.log("A+");
+}
+else if (marks >= 70){
+    console.log("A-");
+}
+else if (marks >=60){
+    console.log("B")
+}
+else if (marks>=33){
+    console.log("C");
+}
+else {
+    console.log("Fail");
+}
+
+}
+else{
+    console.log("Please enter marks Between 0 to 100")
+}
